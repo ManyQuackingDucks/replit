b09f6dc042934416573ce3e616bafddc6f3ff09d
Your Name <you@example.com>
2022-01-21 19:43:52 +0000

    Changes to cover all code

--- tests/db_test.rs
+++ tests/db_test.rs
@@ -12,6 +12,8 @@ async fn test() {
     assert_eq!(db.get("k").await.unwrap(), "v");
     let list = db.list(None).await.unwrap();
     assert_eq!(list[0], "k");
+    let list = db.list(Some("k")).await.unwrap();
+    assert_eq!(list[0], "k");
     db.delete("k").await.unwrap();
 }
 
@@ -27,7 +29,7 @@ async fn test_get(){
 #[should_panic]
 async fn test_list(){
     let _guard = TEST_LOCK.lock();
-    let db = replit::db::Db::new().unwrap();
+    let db = replit::db::Db::new_with_url("https://example.com".to_string());
     let list = db.list(None).await.unwrap();
     assert_eq!(list[0], "k");
 }
